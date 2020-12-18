837d7236e9c27c34d1b0b074410e662d5596b1e0
Valentin Kuznetsov <vkuznet@gmail.com>
2020-12-18 13:48:19 -0500

    update

--- k8s_info.go
+++ k8s_info.go
@@ -7,10 +7,8 @@ package main
 
 import (
 	"encoding/json"
-	"flag"
 	"fmt"
 	"log"
-	"net"
 	"os/exec"
 	"strings"
 )
@@ -57,32 +55,7 @@ type PodInfo struct {
 	//     Kind       string   `json:"Kind"`
 	Metadata Metadata `json:"Metadata"`
 	//     Spec       Spec     `json:"Spec"`
-	Status Status `json:Status`
-}
-
-func (p *PodInfo) Info(verbose int) string {
-	var info string
-	//     fmt.Printf("pod info %+v\n", p)
-	//     fmt.Printf("pod status %+v\n", p.Status)
-	for _, cs := range p.Status.ContainerStatuses {
-		//         fmt.Println("status", cs)
-		info += fmt.Sprintf("name       : %s\n", cs.Name)
-		info += fmt.Sprintf("image      : %s\n", cs.Image)
-		if verbose > 0 {
-			info += fmt.Sprintf("imageID    : %s\n", cs.ImageID)
-			info += fmt.Sprintf("containerID: %s\n", cs.ContainerID)
-		}
-	}
-	if verbose > 0 {
-		host, err := net.LookupAddr(p.Status.HostIP)
-		if err == nil {
-			info += fmt.Sprintf("Host       : %s\n", host)
-		} else {
-			info += fmt.Sprintf("HostIP     : %s\n", p.Status.HostIP)
-			info += fmt.Sprintf("PodIP      : %s\n", p.Status.PodIP)
-		}
-	}
-	return info
+	//     Status Status `json:Status`
 }
 
 // helper function to execute command
@@ -192,43 +165,4 @@ func clusterInfo(allowed []string) []PodInfo {
 		}
 	}
 	return info
-}
-
-// oldmain function
-func oldmain() {
-	var verbose int
-	flag.IntVar(&verbose, "verbose", 0, "verbosity level")
-	var ns string
-	flag.StringVar(&ns, "ns", "", "k8s namespace")
-	var pod string
-	flag.StringVar(&pod, "pod", "", "k8s pod")
-	flag.Parse()
-	if pod != "" && ns != "" {
-		p, err := podInfo(pod, ns)
-		if err == nil {
-			fmt.Println(p.Info(verbose))
-		} else {
-			fmt.Println("pod", pod, "error", err)
-		}
-	} else {
-		nss, _ := namespaces()
-		if ns != "" {
-			nss = []string{ns}
-		}
-		for _, ns := range nss {
-			fmt.Println("namespace:", ns)
-			pods, err := pods(ns)
-			if err != nil {
-				panic(err)
-			}
-			for _, pod := range pods {
-				p, err := podInfo(pod, ns)
-				if err == nil {
-					fmt.Println(p.Info(verbose))
-				} else {
-					fmt.Println("pod", pod, "error", err)
-				}
-			}
-		}
-	}
 }
