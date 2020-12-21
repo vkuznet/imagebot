daf971ca6b02c75742f77d8a3ff441eb70e0cc26
Valentin Kuznetsov <vkuznet@gmail.com>
2020-12-21 13:09:36 -0500

    script to test the server workflow

---
+++ test_server.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo "obtain token"
+token=`curl -s -X POST -H "content-type: application/json" -d '{"commit":"dad51084ea82ab2f6f573b6daa464ed0d7c23a1d", "namespace": "http", "repository": "vkuznet/httpgo", "workflow": "test", "tag":"00.00.01", "service":"httpgo"}' http://localhost:8111/token`
+echo "new token $token"
+echo ""
+echo "should fail with wrong token"
+curl -s -X POST -H "Authorization: Bearer xxx$token" -H "content-type: application/json" -d '{"commit":"dad51084ea82ab2f6f573b6daa464ed0d7c23a1d", "namespace": "http", "repository": "vkuznet/httpgo", "workflow": "test", "tag":"00.00.01", "service":"httpgo"}' http://localhost:8111
+echo ""
+echo "should fail to process request"
+curl -s -X POST -H "Authorization: Bearer $token" -H "content-type: application/json" -d '{"commit":"dad51084ea82ab2f6f573b6daa464ed0d7c23a1d", "namespace": "http", "repository": "vkuznet/httpgo", "workflow": "test", "tag":"00.00.01", "service":"httpgo"}' http://localhost:8111
