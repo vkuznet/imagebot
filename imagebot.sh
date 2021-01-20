c91064ed5c6743439515d18d2d8555c4c4fd3f0b
Valentin Kuznetsov <vkuznet@gmail.com>
2021-01-20 15:37:29 -0500

    Update

--- imagebot.sh
+++ imagebot.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-token=`curl -v -s -X POST -H "content-type: application/json" -d '{"commit":"COMMIT", "namespace": "NAMESPACE", "repository": "REPOSITORY", "image": "IMAGE", "tag":"TAG", "service":"SERVICE"}' HOST/token`
+token=`curl -k -s -X POST -H "content-type: application/json" -d '{"commit":"COMMIT", "namespace": "NAMESPACE", "repository": "REPOSITORY", "image": "IMAGE", "tag":"TAG", "service":"SERVICE"}' HOST/token`
 echo "TOKEN: $token"
-curl -v -s -X POST -H "Authorization: Bearer $token" -H "content-type: application/json" -d '{"commit":"COMMIT", "namespace": "NAMESPACE", "repository": "REPOSITORY", "image": "IMAGE",  "tag":"TAG", "service":"SERVICE"}' HOST/
+curl -k -s -X POST -H "Authorization: Bearer $token" -H "content-type: application/json" -d '{"commit":"COMMIT", "namespace": "NAMESPACE", "repository": "REPOSITORY", "image": "IMAGE",  "tag":"TAG", "service":"SERVICE"}' HOST/
